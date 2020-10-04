88844573d77c8c7c7c33150572df521ade87b636
amandacutrer <23acutrer@athenian.org>
2020-10-04 18:33:21 +0000

    finished probability code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -19,36 +19,44 @@ public class MyMain {
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
-    public static double probabilityTwoSixes() {
+    public static double probabilityTwoSixes() { 
         int trial = 0;
         for (int i = 0; i < 10000; i++){
             int sixes = 0;
             for(int j = 0; j < 12; j++){
                 if ((int)((Math.random() * 6)+1) == 6){
-                    sixes += sixes;
+                    sixes++;
                 }
+            }
                     if (sixes >= 2){
                     trial++;
-                    break;
                     }
-                
             }
-    }
-
-    System.out.println(trial);
         return (double)trial / (double)(10000) * 100;
-    }
+    } 
 
     // Calculate the probability of rolling at least three 6's when rolling 
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int trial = 0;
+        for (int i = 0; i < 10000; i++){
+            int sixes = 0;
+            for(int j = 0; j < 18; j++){
+                if ((int)((Math.random() * 6)+1) == 6){
+                    sixes++;
+                }
+            }
+                    if (sixes >= 3){
+                    trial++;
+                    }
+            }
+        return (double)trial / (double)(10000) * 100;
     }
 
 
     public static void main(String[] args) {
         System.out.println(probabilityOneSix());
         System.out.println(probabilityTwoSixes());
+        System.out.println(probabilityThreeSixes());
     }
 }
